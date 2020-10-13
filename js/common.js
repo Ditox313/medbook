f2acfb7e93eb33b8af994f4c04b140036674f425
Xsodex <flex513513@gmail.com>
2020-10-13 17:04:42 +0300

    Начало верстки

---
+++ js/common.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Библиотеки
+$(document).ready(function() {
+  new WOW().init();
+});
+
+$(document).ready(function() {
+    $(".fancybox").fancybox({
+    	    "imageScale"            : false,
+            "zoomOpacity"			: true,
+            "overlayShow"			: false,
+            "zoomSpeedIn"			: 500,
+            "zoomSpeedOut"			: 500
+    });
+});
