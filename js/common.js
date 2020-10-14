e2033e55db3b970fb790ebe381a0bebe32ddda93
Xsodex <flex513513@gmail.com>
2020-10-14 22:04:43 +0300

    Добавил скрол до блоков и кнопку наверх

--- js/common.js
+++ js/common.js
@@ -1,40 +1,72 @@
 $(document).ready(function() {
 "use strict";
 
+//Аккордион
+
 $('.questsion_block').on('click', function(){
   $(this).children('.questsion_block_answer').slideToggle('normal');
 })
 
 
 
+ 
+//Кнопка наверх
+  $(function()
+{
+
+	var btnUp = $(".up_but");
+
+	var top = $(this).scrollTop();
+		if( top > 300 ){
+			btnUp.fadeIn(500);
+		}
+		else
+		{
+			btnUp.fadeOut(500);
+		}
+
+		
+	$(document).on("scroll",function()
+	{
+		var top = $(this).scrollTop();
+		if( top > 300 ){
+			btnUp.fadeIn(500);
+		}
+		else
+		{
+			btnUp.fadeOut(500);
+		}
+	});
+
+	btnUp.on("click",function()
+	{
+		$("html,body").animate({
+			scrollTop:0
+		},400);
+	});
+});
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-// Библиотеки
-
+//wow
   new WOW().init();
 
-  // 
 
-    $(".fancybox").fancybox({
-    	    "imageScale"            : false,
-            "zoomOpacity"			: true,
-            "overlayShow"			: false,
-            "zoomSpeedIn"			: 500,
-            "zoomSpeedOut"			: 500
-    });
+// Скрол до блока
 
-    // 
+    $(".link").on("click",function(e)
+	{
+		e.preventDefault();
+
+		var selector = $(this).attr("href"); //получили Атрибут
+		var h = $(selector);  //получили элемент с данным атрибутом
+		
+
+		$("html,body").animate({
+			scrollTop: h.offset().top //мы скролим тег body на высоту h.offset(высоту получаем при клике на элемент меню).top
+		},1000)
+	});
 
   });
